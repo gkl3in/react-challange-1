fa46176b4e502eb4ad94b83bff7f6e9c07df0144
Gabriel Klein <gabriel.klein@philips.com>
2023-01-10 14:57:26 -0300

    Primeiros testes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 /*
 Consuma a API e liste todos os pokemons da consulta do seguinte endpoint. 
@@ -22,11 +23,45 @@ EXTRA: se puder ordene por nome.
 
 function App() {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState({
+    name: '',
+    baseExperience: '',
+    frontDefault: ''
+  });
+
+  const fetchData = () => {
+    return axios.get("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => {
+        let res = response.data.results.map((result) => {
+          return fetchDataChildren(result.url);
+
+        });
+        console.log(res);
+      });
+  }
+
+  const fetchDataChildren = (url) => {
+    return axios.get(url)
+      .then((response) => {
+        return response.data.name
+  
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <h3>desafio fernandev</h3>
       <h1>consumir api pokémon</h1>
+      {/* {user?.map(user => (
+        <div>
+          <img src="https://www.tutorialspoint.com/assets/questions/media/426142-1668760872.png"></img>
+          <p>{user.name}</p>
+        </div>
+      ))} */}
     </>
   );
 }
